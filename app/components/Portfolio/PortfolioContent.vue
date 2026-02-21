259bc14c5d841271d3273dd996f479916a2d7ae6
マーボーのSURFA\hamam <develop.masaharu@gmail.com>
2026-02-21 18:03:35 +0900

    内容表示コンポーネント作成

---
+++ app/components/Portfolio/PortfolioContent.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-dialog max-width="400">
+    <template #activator="{ props: activatorProps }">
+      <v-btn class="h-auto w-auto py-4" v-bind="activatorProps">
+        <v-img
+          :width="250"
+          aspect-ratio="16/9"
+          :src="props.imgUrl"
+          style="cursor: pointer"
+        ></v-img>
+      </v-btn>
+    </template>
+    <template #default="{ isActive }">
+      <v-card max-width="400">
+        <v-card-title>
+          <v-row>
+            <v-col cols="10">
+              <h3>{{ props.title }}</h3>
+            </v-col>
+            <v-col cols="2">
+              <v-btn
+                icon="mdi-close"
+                variant="plain"
+                size="small"
+                @click.stop="isActive.value = false"
+              >
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card-title>
+        <v-card-text>
+          <p>{{ props.description }}</p>
+        </v-card-text>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+  description: string
+  imgUrl: string
+  link?: string
+}>()
+</script>
